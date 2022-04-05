4c62b2b515faa043f982140ee9da6fba07d8423f
Martín <magadiflo@gmail.com>
2022-04-05 12:51:41 -0500

    Event bindings bi-direccionales

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -10,6 +10,9 @@ export class HelloComponent implements OnInit {
   @Input() name!: string;
   @Output() sayHello: EventEmitter<string> = new EventEmitter();
 
+  namesList: string[] = [];
+  inputName: string = 'asdf';
+
   constructor() { }
 
   ngOnInit(): void {
@@ -17,6 +20,17 @@ export class HelloComponent implements OnInit {
 
   onNameClick(): void {
     this.sayHello.emit('Hola desde el componente hijo hello!!');
+  }
+
+  addName(): void {
+    this.namesList.push(this.inputName);
+    this.inputName = '';
+    console.log(this.namesList);  
+  }
+
+  updateInputName(event: any): void {
+    this.inputName = event.target.value;
+    console.log(this.inputName);  
   }
 
 }
