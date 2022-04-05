0d107eb2808cf5f12b5b17ddd419e27a6803b989
Martín <magadiflo@gmail.com>
2022-04-05 12:02:04 -0500

    Decorador @Input

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelloComponent implements OnInit {
 
-  public name: string = 'Martín';
+  @Input() name!: string;
 
   constructor() { }
 
