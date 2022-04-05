cbbc0ce7ffbca7a3d44f62456135873c2e4ea853
Martín <magadiflo@gmail.com>
2022-04-05 12:08:26 -0500

    Property bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-school-c1';
+
+  myName: string = 'Martín Díaz';
+
 }
