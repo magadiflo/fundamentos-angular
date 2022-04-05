2888b2021188ac956167dd5089b468ea99e49152
Martín <magadiflo@gmail.com>
2022-04-05 11:31:02 -0500

    Creando un componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HelloComponent } from './hello/hello.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
