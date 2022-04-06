66392b437f3d0c96df52f7f372474f144ee7d15d
Martín <magadiflo@gmail.com>
2022-04-06 10:21:09 -0500

    Directiva ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  myName: string = 'Martín Díaz';
-
+  
   onSayHello(sayHello: string) {
     console.log(sayHello);  
   }
